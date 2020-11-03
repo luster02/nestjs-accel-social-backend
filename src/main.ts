aa3df09434be1858f9e45e0878d59b90fc8eb383
luster02 <bombolti_02@hotmail.com>
2020-11-02 20:52:47 -0600

    [ADD]: app middlewares

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import * as helmet from 'helmet';
+import * as compression from 'compression';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors()
+  app.use(helmet())
+  app.use(compression())
   await app.listen(AppModule.port);
 }
 bootstrap();
