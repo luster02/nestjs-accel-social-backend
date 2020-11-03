7a69bcaebe20ebb49f0c783e0a445bf0a2bf1fd2
luster02 <bombolti_02@hotmail.com>
2020-11-02 19:11:36 -0600

    [ADD]: app module & bootstrap

---
+++ src/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config'
+import { AppController } from './app.controller';
+import { ConfigModule } from './config/config.module'
+import { DatabaseModule } from './database/database.module'
+import { IEnvironmentVariables } from './config/config.interface'
+import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { GqlModule } from './gql/gql.module';
+import { PostModule } from './modules/post/post.module';
+import { CommentModule } from './modules/comment/comment.module';
+import { ChatModule } from './modules/chat/chat.module';
+
+@Module({
+  imports: [
+    ConfigModule,
+    DatabaseModule,
+    UserModule,
+    AuthModule,
+    GqlModule,
+    PostModule,
+    CommentModule,
+    ChatModule
+  ],
+  controllers: [AppController],
+})
+export class AppModule {
+  static port: number;
+  constructor(private configService: ConfigService<IEnvironmentVariables>) {
+    AppModule.port = this.configService.get<number>('PORT')
+  }
+}
