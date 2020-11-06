6df6e493af7da80d5bb1d9cd01b7779445fe75ea
luster02 <bombolti_02@hotmail.com>
2020-11-06 17:39:56 -0600

    [ADD]: config keys

--- src/config/config.interface.ts
+++ src/config/config.interface.ts
@@ -2,4 +2,7 @@ export interface IEnvironmentVariables {
     PORT: number;
     JWT_SECRET: string;
     MONGODB_URI: string;
+    CLOUD_NAME: string;
+    API_KEY: string;
+    API_SECRET: string;
 }
--- src/config/config.provider.ts
+++ src/config/config.provider.ts
@@ -1,5 +1,8 @@
 export default () => ({
     PORT: parseInt(process.env.PORT),
     JWT_SECRET: process.env.JWT_SECRET,
-    MONGODB_URI: process.env.MONGODB_URI
+    MONGODB_URI: process.env.MONGODB_URI,
+    CLOUD_NAME: process.env.CLOUD_NAME,
+    API_KEY: process.env.API_KEY,
+    API_SECRET: process.env.API_SECRET,
 })
