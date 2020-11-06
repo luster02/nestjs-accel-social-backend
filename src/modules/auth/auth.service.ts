71f2bb64bd42e1f8103df8a58024d845bc2a4b5f
luster02 <bombolti_02@hotmail.com>
2020-11-06 17:41:54 -0600

    [ADD]: jwt in signup method

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -11,29 +11,45 @@ import { Model } from 'mongoose'
 import { hash, compare } from 'bcryptjs'
 import { IJwtPayload } from './interfaces'
 import { User } from '@user/schemas/user.schema'
+import { UserDetails } from '@user/schemas/user.detail.shcema'
 import { SignupDto, SigninDto } from './dto'
-
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectModel('User')
         private readonly userModel: Model<User>,
+        @InjectModel('UserDetails')
+        private readonly userDetails: Model<UserDetails>,
         private readonly _jwtService: JwtService,
     ) { }
 
-    async signup(userData: SignupDto) {
+    async signup(userData: SignupDto): Promise<string> {
         const { password } = userData
         const hashedPassword = await hash(password, 10)
-        const user = new this.userModel({ ...userData, password: hashedPassword })
+        const user: User = new this.userModel({ ...userData, password: hashedPassword })
 
         try {
             await user.save()
+            const details = new this.userDetails({
+                foler_name: user._id,
+                user: user._id
+            })
+            details.save()
+            
+            const payload: IJwtPayload = {
+                id: user.id,
+                email: user.email,
+                username: user.username
+            };
+
+            return this._jwtService.sign(payload);
+
         } catch (error) {
             if (parseInt(error.code) === 11000) {
                 throw new ConflictException('User or email already exists');
             }
-            
+
             throw new InternalServerErrorException()
         }
 
@@ -52,8 +68,6 @@ export class AuthService {
             username: user.username
         };
 
-        const token = this._jwtService.sign(payload);
-
-        return token;
+        return this._jwtService.sign(payload);
     }
 }
